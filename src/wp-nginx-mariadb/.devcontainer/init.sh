1a7b24e4aca6c02969f0820cd0161510644a0045
SGr33n <sdefalco@gmail.com>
2024-08-03 11:36:43 +0200

    Trying a new approach to ask options

--- src/wp-nginx-mariadb/.devcontainer/init.sh
+++ src/wp-nginx-mariadb/.devcontainer/init.sh
@@ -5,8 +5,10 @@ set -eux
 WP_VERSION="WordPress Version > "
 
 WP_PATH="$1"
+WP_VERSION="$2"
 echo "\
-VER=0.1.7
+VER=0.1.8
 USER_UID=1000
 WP_PATH=$WP_PATH
+WP_VERSION=$WP_VERSION
 " > .devcontainer/.env
